4e4f7a24af7602a1405beec756b54810b482c189
Juwan Lee <ljw05w5@gmail.com>
2023-04-17 12:01:15 +0900

    Thread.Sleep 연습하기

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApp
@@ -10,10 +11,12 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            //절대값 기준
+            // 절대값 기준
             Console.Write("메서드 호출");
+            Thread.Sleep(1000); // 시간 정확하지 않음
             Console.SetCursorPosition(5, 5);
             Console.Write("메서드 호출");
+            Thread.Sleep(2000);
             Console.SetCursorPosition(3, 2);
             Console.Write("메서드 호출");
         }
