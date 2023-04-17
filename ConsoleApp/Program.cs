882d807c53c49a6dc617568a707b112da6503ed4
Juwan Lee <ljw05w5@gmail.com>
2023-04-17 11:58:04 +0900

    SetCursorPosition 연습하기

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,6 +10,12 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            //절대값 기준
+            Console.Write("메서드 호출");
+            Console.SetCursorPosition(5, 5);
+            Console.Write("메서드 호출");
+            Console.SetCursorPosition(3, 2);
+            Console.Write("메서드 호출");
         }
     }
 }
