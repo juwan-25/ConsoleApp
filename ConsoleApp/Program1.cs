1743152644652529e55d02b667e09aeee43890bb
Juwan Lee <ljw05w5@gmail.com>
2023-04-17 12:14:43 +0900

    콘솔 메서드 활용하기

---
+++ ConsoleApp/Program1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Program1
+    {
+        static void Main(string[] args)
+        {
+
+
+            int x = 1;
+            while (x < 50)
+            {
+                Console.Clear();
+                Console.SetCursorPosition(x, 5);
+
+                if (x % 3 == 0)
+                    Console.WriteLine("__0");
+                else if (x % 3 == 1)
+                    Console.WriteLine("_^0");
+                else
+                    Console.WriteLine("^_0");
+
+                Thread.Sleep(1000);
+                x++;
+            }
+        }
+    }
+}
